76ce108e7251b5a25601ffb4df086f5f3431af9f
Eduardo Alba <eduardoandresalba@gmail.com>
2024-04-07 18:39:00 -0400

    (fix) added resizing compatibility

--- app/page.js
+++ app/page.js
@@ -6,51 +6,63 @@ import ScuttleImage from "@/public/scuttle.png";
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col p-24 ${robotoMono.className} gap-12 max-w-screen-xl mx-auto`}
+      className={`flex min-h-screen p-12 md:p-16 lg:p-24 flex-col ${robotoMono.className} max-w-screen-xl mx-auto`}
     >
-      {/* Header */}
-      <h1 className="text-6xl w-2/3">
-        Software Engineer creating interactive online experiences.
-      </h1>
+      {/* Info */}
+      <div className="flex flex-col gap-16">
+        {/* Header */}
+        <div className="">
+          <h1 className="text-4xl sm:text-5xl lg:text-7xl">
+            Software Engineer creating interactive online experiences.
+          </h1>
+        </div>
 
-      {/* Description */}
-      <div className="flex flex-col gap-6 mt-6 opacity-70">
-        <p className="text-xl">
-          I'm Eduardo, a US based software engineer with two years of work
-          experience and four years of hobbiest react experience. I specialize
-          in creating web applications with a focus on user experience and
-          practical utility.
-        </p>
+        {/* Description */}
+        <div className="flex flex-col gap-16 opacity-70 text-base lg:text-xl">
+          <p>
+            I'm Eduardo, a US based software engineer with two years of work
+            experience and four years of hobbiest react experience. I specialize
+            in creating web applications with a focus on user experience and
+            practical utility.
+          </p>
 
-        <p className="text-xl">
-          I am currently working for{" "}
-          <span className="underline">Barracuda Networks</span> on the XDR team
-          as a full stack software engineer where I help shape the future of
-          cybersecurity. I have contributed to a number of projects spanning
-          from developing APIs for external partners to implementing interactive
-          device monitorization tools for added security.
-        </p>
+          <p>
+            I am currently working for{" "}
+            <span>
+              <a
+                href="https://www.barracuda.com/"
+                className="underline hover:text-white hover:font-bold ease-in duration-150"
+              >
+                Barracuda Networks
+              </a>
+            </span>{" "}
+            on the XDR team as a full stack software engineer where I help shape
+            the future of cybersecurity. I have contributed to a number of
+            projects spanning from developing APIs for external partners to
+            implementing interactive device monitorization tools for added
+            security.
+          </p>
+        </div>
+        {/* Links */}
+        <div className="flex items-center gap-4">
+          <EmailMe />
+        </div>
       </div>
 
-      {/* Links */}
-      <div className="flex items-center gap-4">
-        <EmailMe />
-      </div>
-
-      <hr className="opacity-25" />
+      <hr className="opacity-25 my-12" />
 
       {/* Projects */}
       <div className="flex flex-col gap-2">
-        <h2 className="text-3xl font-semibold">Featured Projects</h2>
-        <p className="opacity-50">
+        <h2 className="text-xl md:text-3xl font-semibold">Featured Projects</h2>
+        <p className="opacity-50 text-sm md:text-base">
           A collection of side projects that have shipped recently.
         </p>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mt-6">
         <ProjectCard
           title="Scuttle"
-          description="A League of Legends discord bot for fetching and displaying match data. Scuttle periodically retrieves and stores League of Legends match data to run detailed statistical comparisons. Each server has its own summoners and its own data."
+          description="A League of Legends discord bot for fetching and displaying match data."
           icon={ScuttleImage}
           link="https://www.scuttle.gg"
         />
--- app/ui/components/ProjectCard.js
+++ app/ui/components/ProjectCard.js
@@ -3,13 +3,15 @@ import Image from "next/image";
 function ProjectCard({ title, description, icon, link }) {
   return (
     <a href={link}>
-      <div className="rounded-xl bg-zinc-800 flex p-6 gap-6 hover:bg-transparent border-2 border-transparent hover:border-zinc-800 ease-in duration-150">
-        <div className="w-[150px]">
-          <Image src={icon} alt={title} className="rounded-full" />
-        </div>
-        <div className="flex flex-col gap-1">
-          <h3 className="text-lg opacity-85">{title}</h3>
-          <p className="text-base opacity-50">{description}</p>
+      <div className="rounded-xl bg-zinc-800 flex items-center p-4 gap-6 hover:bg-transparent border-2 border-transparent hover:border-zinc-800 ease-in duration-150">
+        <Image
+          src={icon}
+          alt={title}
+          className="rounded-full w-[45px] h-[45px]"
+        />
+        <div className="flex flex-col">
+          <h3 className="text-base md:txt-lg font-bold opacity-85">{title}</h3>
+          <p className="text-sm md:text-base opacity-60">{description}</p>
         </div>
       </div>
     </a>
