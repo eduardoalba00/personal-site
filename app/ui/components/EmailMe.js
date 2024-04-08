c7e615610fc7d24507603df6f214f20a91ac1bc2
Eduardo Alba <eduardoandresalba@gmail.com>
2024-04-07 22:01:04 -0400

    (new) added icon and static email

--- app/ui/components/EmailMe.js
+++ app/ui/components/EmailMe.js
@@ -1,9 +1,5 @@
 function EmailMe() {
-  return (
-    <button className="rounded-2xl px-4 py-2 bg-zinc-800 font-bold hover:bg-zinc-100 hover:text-black ease-in duration-150">
-      Email Me
-    </button>
-  );
+  return <p className="opacity-50">Email: eduardoandresalba@gmail.com</p>;
 }
 
 export default EmailMe;
